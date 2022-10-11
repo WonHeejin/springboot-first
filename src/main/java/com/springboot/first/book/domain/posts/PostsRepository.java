f0a6ad481efe3ed50fd8fb517e7e3ca58743241f
wonheejin <youth772@naver.com>
2022-10-11 16:53:52 +0900

    전체 조회 화면 만들기

--- src/main/java/com/springboot/first/book/domain/posts/PostsRepository.java
+++ src/main/java/com/springboot/first/book/domain/posts/PostsRepository.java
@@ -1,8 +1,15 @@
 package com.springboot.first.book.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    // SpringDataJpa에서 제공하지 않는 메소드는 @Query로 작성
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
 /*
 * ibatis나 Mybatis 등에서 Dao라고 불리는 DB Layer 접근자
--- src/main/java/com/springboot/first/book/web/IndexController.java
+++ src/main/java/com/springboot/first/book/web/IndexController.java
@@ -1,15 +1,22 @@
 package com.springboot.first.book.web;
 
+import com.springboot.first.book.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
     //머스테치 스타터로 인해 컨트롤러에서 문자열을 반환할 때 앞의 경로와 뒤의 파일 확장자는 자동으로 지정됨
     //앞의 경로: src/main/resources/templates
     //뒤의 파일 확장자: .mustache
+
+    private final PostsService postsService;
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
